34bbdb42ccaddb1c9b49caab75866841f46f4334
beinatlanda <34027309+beinatlanda@users.noreply.github.com>
2018-09-16 22:17:55 +0200

    Improved branding

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Trackr</h1>
+      <h1 className="box-layout__title">Trackr<span className="logo-dot">.</span></h1>
       <p>Track your expenses the easy way.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
